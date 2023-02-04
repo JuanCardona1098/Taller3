39ce3e44c6f4f778327071222c859e64daa4d518
Juan Cardona <juancardona1098@gmail.com>
2023-02-03 22:35:09 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+let db;
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/api/1", async (req, res) => {
+  const result = await db
+    .collection("companies")
+    .find({
+      email_address: { $regex: "@twitter.com" }
+    })
+    .limit(20)
+    .toArray();
+  res.send(result);
+});
+
+app.get("/api/2", async (req, res) => {
+    const result = await db
+      .collection("companies")
+      .find({
+        founded_year: { $gte: 2005, $lte: 2008 },
+      })
+      .limit(20)
+      .toArray();
+    res.send(result);
+  });
+
+
+app.get("/api/3", async (req, res) => {
+  const result = await db
+    .collection("companies")
+    .find({ name: /Technorati/ })
+    .limit(5)
+    .toArray();
+
+  res.send(result);
+});
+
+
+app.get("/api/4", async (req, res) => {
+  const result = await db
+    .collection("companies")
+    .find({
+      $and: [{ category_code: /advertising/ }, 
+      { founded_year: { $eq: 2002 } 
+    }],
+    })
+    .limit(5)
+    .toArray();
+
+  res.send(result);
+});
+
+
+app.get("/api/5", async (req, res) => {
+  const result = await db
+    .collection("companies")
+    .find({
+      $or: [{ category_code: /messaging/ }, 
+      { category_code: /games_video/
+     }],
+    })
+    .limit(5)
+    .toArray();
+
+  res.send(result);
+});
+
+
+app.get("/api/6", async (req, res) => {
+  try {
+    console.log(req.query);
+    const founded_year = parseInt(req.query.founded_year);
+
+    const result = await db
+      .collection("companies")
+      .find({
+        founded_year,
+      })
+      .limit(5)
+      .toArray();
+
+    res.status(200).json({ ok: true, data: result });
+  } catch (error) {
+    res.status(400).json({ ok: false, message: error.message });
+  }
+});
+
+app.post("/api/7", async (req, res) => {
+  try {
+    console.log(req.body);
+    const result = await db
+      .collection("companies")
+      .find(req.body)
+      .limit(5)
+      .toArray();
+
+    res.status(200).json({ ok: true, data: result });
+  } catch (error) {
+    res.status(400).json({ ok: false, message: error.message });
+  }
+});
+mongoose
+  .connect(
+    "mongodb+srv://juan:hola@cluster0.ios47dj.mongodb.net/sample_training?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Mongo DB Connected!");
+    db = mongoose.connection.db;
+  })
+  .catch(() => {
+    console.log("Connection Failed!");
+  });
+// .finally(()=>{
+//   console.log("Request finished!");
+// });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
